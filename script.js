f8008169354582f2b5a9c4e9e2ed94a50ac9705f
Diane Galiano <dianegaliano09@gmail.com>
2021-08-14 20:56:20 -0500

    changed to have var for lower case letters, uppercase letters, and numbers. var names passwordAlphabet,passwordCapAlphabet, and passwordNumb.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,21 +17,10 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 
 }
-function writePassword(); {
-const password = "abcefghijklmnopqrstuvwxyz"
+var passwordAlphabet = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
 
-return password[Math.floor(Math.random() * alphabet.lenghth)]
-}
-console.log(writePassword())
+var passwordCapAlphabet = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"]
 
-function writePassword(); {
-  const password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordSpecialChar = ["!,@,#,$,%,^,&,*,?"]
 
-  return password[Math.floor(Math.random() *alphabet.length)]
-}
-
-function generatePassword(event) {
-  event.preventDefault();
-  console.log(event);
-  var passwordText = "How many characters does your password need?" + 
-}
+var passwordNumb = ["0,1,2,3,4,5,6,7,8,9"]
